60558b5f4ffabf690d4eb3c6761be0bfbd4f3e67
junoade <ajchoi0928@gmail.com>
2026-04-01 21:31:10 +0900

    feat(wallet): 도메인 인터페이스 및 JPA 구현체 개발 Refs: #5

---
+++ src/main/java/com/lemonpay/wallet/infrastructure/WalletJpaRepository.java
@@ -0,0 +1,15 @@
+package com.lemonpay.wallet.infrastructure;
+
+import com.lemonpay.member.domain.Member;
+import com.lemonpay.wallet.domain.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface WalletJpaRepository extends JpaRepository<Wallet, UUID> {
+    Optional<Wallet> findByMemberId(UUID memberId);
+    Optional<Wallet> findByMember(Member member);
+    boolean existsByMemberId(UUID memberId);
+    boolean existsByMember(Member member);
+}
